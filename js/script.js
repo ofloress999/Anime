300320abbb0e1b6d56681155c602f1766d96b362
Igor Flores <contato.ofloress999@gmail.com>
2025-03-11 22:41:30 -0300

    add event click
    Adicionando evento de click para troca de divs.

--- js/script.js
+++ js/script.js
@@ -3,12 +3,18 @@ document.addEventListener("DOMContentLoaded", function() {
     const registerForm = document.querySelector(".register-form");
     const loginForm = document.querySelector(".login-form");
     const buttonRegister = document.querySelector(".button-register");
+    const infoLeft = document.querySelector(".leftside");
+    const infoLeftR = document.querySelector(".leftside-r");
+    const rightContainer = document.querySelector(".right-container");
 
     buttonLogin.addEventListener("click", function (e) {
         e.preventDefault();
 
         if(registerForm.style.display === "flex") {
             registerForm.style.display = "none";
+            rightContainer.style.backgroundColor = "#000";
+            infoLeft.style.display = "none"
+            infoLeftR.style.display = "Flex"
             loginForm.style.display = "block";
         } else {
             registerForm.style.display = "flex";
@@ -16,4 +22,19 @@ document.addEventListener("DOMContentLoaded", function() {
         }
 
     });
+
+    buttonRegister.addEventListener("click", function (e) {
+        e.preventDefault();
+
+        if(registerForm.style.display === "none") {
+            registerForm.style.display = "flex";
+            rightContainer.style.backgroundColor = "#fff";
+            infoLeft.style.display = "flex"
+            infoLeftR.style.display = "none"
+            loginForm.style.display = "none";
+        } else {
+            registerForm.style.display = "none";
+            loginForm.style.display = "flex";
+        }
+    }); 
 });
